3ad58ad3f8932fa59fe691619fd1fd8419e7a650
sushmareddykoncha <koncha.sushma.reddy@ibm.com>
2022-03-08 08:56:41 +0530

    Day 6

---
+++ ConsoleApp2/AbstractClasses.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class AbstractClasses
+    {
+        abstract class Car
+        {
+            public abstract void carType();
+            public void sleep()
+            {
+                Console.WriteLine("Zzz");
+            }
+        }
+
+        class Audi : Car
+        {
+            public override void carType()
+            {
+                Console.WriteLine("The car type: Sedan");
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Audi car = new Audi();
+                car.carType();
+                car.sleep();
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/InterfaceExample.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class InterfaceExample
+    {
+        interface ICar
+        {
+            void carType();
+        }
+
+        class Audi : ICar
+        {
+            public void carType()
+            {
+                Console.WriteLine("The car Type is Sedan");
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Audi car = new Audi();
+                car.carType();
+            }
+        }
+    }
+}
